18011758381e3d99dfaaba69aa904ae55a167e04
dajahu <dajahu@gmail.com>
2017-02-05 22:03:56 +0100

    1.0

---
+++ test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by David Hultgren on 2016-03-02.
+//  Copyright © 2016 The PainSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func mybutton(sender: AnyObject) {
+       
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
